e050fb54dd5639fc83f7d2c62b5f575dac194f31
Julia (Juli) Huang <92133615+TheClassicTechno@users.noreply.github.com>
2025-05-28 22:59:45 -0700

    Create nwconnection.swift

---
+++ nwconnection.swift
@@ -0,0 +1,82 @@
+// Created by juli huang on 5/28/25.
+
+import Foundation
+import Network
+
+/// Sends an HTTP GET request over the given connection.
+func sendHTTPRequest(on connection: NWConnection) {
+    let requestString = """
+    GET / HTTP/1.1\r\n
+    Host: www.apple.com\r\n
+    Connection: close\r\n
+    \r\n
+    """
+    guard let requestData = requestString.data(using: .utf8) else {
+        
+        return
+    }
+    connection.send(content: requestData, completion: .contentProcessed { error in
+        if let error = error {
+            
+            exit(EXIT_FAILURE)
+        }
+    })
+}
+
+/// Recursively receives chunks of Data until the connection is closed.
+func receiveHTTPResponse(on connection: NWConnection) {
+    connection.receive(minimumIncompleteLength: 1, maximumLength: 4096) { data, _, isComplete, error in
+        if let error = error {
+            
+            exit(EXIT_FAILURE)
+        }
+        if let data = data, !data.isEmpty {
+            // Convert the Data chunk directly into a String
+            if let text = String(data: data, encoding: .utf8) {
+                print(text, terminator: "")
+            } else {
+                print("cant decode chunk")
+            }
+        }
+        if isComplete {
+           
+            connection.cancel()
+            exit(EXIT_SUCCESS)
+        } else {
+            // Keep reading more data
+            receiveHTTPResponse(connection: connection)
+        }
+    }
+}
+
+func main() {
+    let connection = NWConnection(
+        host: .init("www.apple.com"),
+        port: .init(integerLiteral: 80),
+        using: .tcp
+    )
+
+    connection.stateUpdateHandler = { state in
+        switch state {
+        case .waiting(let error):
+            print("connection error: \(error)")
+        case .ready:
+          
+            sendHTTPRequest(on: connection)
+            receiveHTTPResponse(on: connection)
+        case .failed(let error):
+          
+            exit(EXIT_FAILURE)
+        case .cancelled:
+           
+            exit(EXIT_SUCCESS)
+        default:
+            break
+        }
+    }
+
+    connection.start(queue: .main)
+    dispatchMain()  // keeps the process alive for callbacks
+}
+
+main()
